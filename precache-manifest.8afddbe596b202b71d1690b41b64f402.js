9d6012a360c819d61d7cf91e6b776f3d62c910e5
AnuradhaSD <anuradha.sanjeewa@my.sliit.lk>
2020-04-23 14:48:51 +0530

    Updates

---
+++ precache-manifest.8afddbe596b202b71d1690b41b64f402.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4315ae83c15f3ab96890d57adc1fdfd1",
+    "url": "/luckySigma/to_do_react_app.git/index.html"
+  },
+  {
+    "revision": "eac9070757bac5ec2d7a",
+    "url": "/luckySigma/to_do_react_app.git/static/css/main.b93a67c6.chunk.css"
+  },
+  {
+    "revision": "48de76597e87878bfc24",
+    "url": "/luckySigma/to_do_react_app.git/static/js/2.fe8c9100.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luckySigma/to_do_react_app.git/static/js/2.fe8c9100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac9070757bac5ec2d7a",
+    "url": "/luckySigma/to_do_react_app.git/static/js/main.abcdfc05.chunk.js"
+  },
+  {
+    "revision": "8ff194fbfaaa33657f01",
+    "url": "/luckySigma/to_do_react_app.git/static/js/runtime-main.fd2119e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luckySigma/to_do_react_app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
